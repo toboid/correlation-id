e9d75a09d7fb36404c98359a99b3a59c54c7007e
Toby <toboid@users.noreply.github.com>
2024-07-17 17:25:48 +0100

    add setId to types

--- index.d.ts
+++ index.d.ts
@@ -3,3 +3,4 @@ export function withId<R>(work: () => R): R;
 export function bindId<R, W extends Function>(id: string, work: W): W;
 export function bindId<R, W extends Function>(work: W): W;
 export function getId(): string | undefined;
+export function setId(id: string): undefined;
